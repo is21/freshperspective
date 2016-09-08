b7fabc93ea49a8468bb502142fa9b3f7fccee765
Ian Schiffer <ischiffer@gmail.com>
2016-09-08 01:33:16 -0700

    cleanup migrations

--- db/migrate/20160804222037_add_username_to_lists.rb
+++
@@ -1,5 +0,0 @@
-class AddUsernameToLists < ActiveRecord::Migration
-  def change
-    add_column :lists, :username, :string
-  end
-end
--- db/migrate/20160808010309_drop_lists_table.rb
+++
@@ -1,9 +0,0 @@
-class DropListsTable < ActiveRecord::Migration
-  def up
-    drop_table :lists
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
