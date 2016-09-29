55e2a553de2940c90a85089f58a2d51bb720c7b0
Ian Schiffer <ischiffer@gmail.com>
2016-09-28 22:13:58 -0700

    add indexes to db

--- db/migrate/20160803013823_create_users.rb
+++ db/migrate/20160803013823_create_users.rb
@@ -3,9 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :provider, null: false
       t.string :uid, null: false
-      # add_index :users, :provider
-      # add_index :users, :uid
-      # add_index :users, [:provider, :uid], unique: true
+       add_index :users, :provider
+       add_index :users, :uid
+       add_index :users, [:provider, :uid], unique: true
       t.string :name
       t.string :location
       t.string :image_url
